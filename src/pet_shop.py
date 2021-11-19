4560959081c321b000e174103d12eb47fee1bdac
petermordaunt <peetfire@hotmail.com>
2021-11-19 11:34:21 +0000

    Solution to ex 3

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -7,3 +7,7 @@ def get_pet_shop_name(pet_shop):
 # Returns the value of 'total_cash' from the pet shop from data structure
 def get_total_cash(pet_shop):
     return pet_shop["admin"]["total_cash"]
+
+# Adds or subtracts the amount passed in to/from the value of 'total_cash'
+def add_or_remove_cash(pet_shop, amount):
+    pet_shop["admin"]["total_cash"] += amount
