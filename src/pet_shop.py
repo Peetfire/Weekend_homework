1d43595f4fd5578c3c36799d5f21e721f252e14e
petermordaunt <peetfire@hotmail.com>
2021-11-19 11:22:14 +0000

    Solution to ex 1

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,1 +1,9 @@
 # WRITE YOUR FUNCTIONS HERE
+
+# Returns the value of 'name' from the pet shop from data structure
+def get_pet_shop_name(pet_shop):
+    return pet_shop["name"]
+
+# Returns the value of 'total_cash' from the pet shop from data structure
+def get_total_cash(pet_shop):
+    return pet_shop["total_cash"]
