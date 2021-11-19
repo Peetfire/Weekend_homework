973a9f264beabd385f8d169d28f03eeed984e8e1
petermordaunt <peetfire@hotmail.com>
2021-11-19 11:44:47 +0000

    Solution to ex 5

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,17 +1,21 @@
 # WRITE YOUR FUNCTIONS HERE
 
-# Returns the value of 'name' from the pet shop from data structure
+# Returns the value of 'name' from the pet_shop from data structure
 def get_pet_shop_name(pet_shop):
     return pet_shop["name"]
 
-# Returns the value of 'total_cash' from the pet shop from data structure
+# Returns the value of 'total_cash' from the pet_shop from data structure
 def get_total_cash(pet_shop):
     return pet_shop["admin"]["total_cash"]
 
-# Adds or subtracts the amount passed in to/from the value of 'total_cash'
+# Adds or subtracts the amount passed in to/from the value of 'total_cash' in the pet_shop from data structure
 def add_or_remove_cash(pet_shop, amount):
     pet_shop["admin"]["total_cash"] += amount
 
-# Returns the value of 'pets_sold" from the pet shop from data structure
+# Returns the value of 'pets_sold" from the pet_shop from data structure
 def get_pets_sold(pet_shop):
     return pet_shop["admin"]["pets_sold"]
+
+# Adds the amount passed in to 'pets_sold' in pet_shop data structure
+def increase_pets_sold(pet_shop, amount):
+    pet_shop["admin"]["pets_sold"] += amount
